81e524fbc34be76a9e25ab508e5a09e542e63e62
Li Haotian <2878348186@qq.com>
2023-02-02 13:26:12 +0800

    update

--- src/cn/lihaotian/bookmanager/utils/DbUtil.java
+++ src/cn/lihaotian/bookmanager/utils/DbUtil.java
@@ -8,9 +8,9 @@ public class DbUtil {
 	public Connection getConnection()throws Exception{
 		String dbDriver = "com.mysql.jdbc.Driver";
 		Class.forName(dbDriver);
-		String dbUrl = "jdbc:mysql://39.107.66.188:3307/bookmanager?useSSL=true&characterEncoding=utf-8";
+		String dbUrl = "jdbc:mysql://127.0.0.1:3306/bookmanager?useSSL=true&characterEncoding=utf-8";
 		String dbUserName = "root";
-		String dbPassword = "root";
+		String dbPassword = "whoami";
 		return (Connection) DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
 	}
 
